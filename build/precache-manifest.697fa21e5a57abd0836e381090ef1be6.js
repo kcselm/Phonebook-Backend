1a249d2239a9efc972a3e9abfc913cd381ace67d
kcselm <kcselm93@gmail.com>
2020-10-06 20:52:00 -0500

    Production build of the frontend

---
+++ build/precache-manifest.697fa21e5a57abd0836e381090ef1be6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd16c3e6cf46186c400a161dac097d6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8533893efce3658618",
+    "url": "/static/css/main.6ce9ec65.chunk.css"
+  },
+  {
+    "revision": "571f1405cf6fe3ef0364",
+    "url": "/static/js/2.7dd20ff8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7dd20ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c8533893efce3658618",
+    "url": "/static/js/main.950086fb.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
